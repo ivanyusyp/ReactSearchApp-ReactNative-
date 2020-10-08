96d2a53316113264c1cc19b6536dcbf3f172985f
ivanyusyp <ivanusup@gmail.com>
2020-10-08 13:10:12 +0300

    reclone

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,7 @@ const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
 				autoCapitalize="none"
 				autoCorrect={false}
 				style={styles.inputStyle}
-				placeholder="Search"
+				placeholder="SearcH"
 				value={term}
 				onChangeText={onTermChange}
 				onEndEditing={onTermSubmit}
